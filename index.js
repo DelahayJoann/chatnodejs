000159eb1e942d316b8ec986dc7527309d09d434
Delahay Joann <delahay.joann@gmail.com>
2021-02-11 16:11:40 +0100

    1

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-const app = require('express')();
+var express = require('express');
+var app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
 app.get('/', function(req,res){
     res.sendFile(__dirname + "/index.html");
+});
+app.get('/css', function(req,res){
+    res.sendFile(__dirname + "/style.css");
+});
+
+http.listen(3000, function(){
+    console.log("localhost:3000");
+});
+
+io.on('connection', function(socket){
+    console.log('connection socket');
+    socket.on('chat', function(msg){
+        io.emit('chat', msg);
+    });
+    socket.on('disconnect', () =>{
+        console.log('disconnected');
+    });
 });
